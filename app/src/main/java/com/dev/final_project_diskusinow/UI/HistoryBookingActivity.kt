615497a1ead8bda864858c1b9cc0552b719515e0
Rizky Al Arief <rizkyalarief11@gmail.com>
2024-03-24 22:00:51 +0700

    feat : fix splash screen

---
+++ app/src/main/java/com/dev/final_project_diskusinow/UI/HistoryBookingActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.final_project_diskusinow.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.final_project_diskusinow.R
+
+class HistoryBookingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_booking)
+    }
+}
