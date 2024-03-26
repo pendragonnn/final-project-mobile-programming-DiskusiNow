fb7a146f9722a07d5d6dc2f6dc28cb4b38cbbc9a
Wisnu Andika <wisnuandika104@gmail.com>
2024-03-26 21:34:27 +0700

    feat: add login part

---
+++ app/src/main/java/com/dev/final_project_diskusinow/UI/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.final_project_diskusinow.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.final_project_diskusinow.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
