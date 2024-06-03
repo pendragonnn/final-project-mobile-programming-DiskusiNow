0c1f7f42d139ffd5956b83654405ff5d9724a7e5
Rizky Al Arief <rizkyalarief11@gmail.com>
2024-06-04 01:16:58 +0700

    feat: room adapter

--- app/src/main/java/com/dev/final_project_diskusinow/UI/roomInformation/RoomInformationActivity.kt
+++ app/src/main/java/com/dev/final_project_diskusinow/UI/roomInformation/RoomInformationActivity.kt
@@ -3,14 +3,21 @@ package com.dev.final_project_diskusinow.UI
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dev.final_project_diskusinow.UI.roomInformation.RoomViewModel
+import com.dev.final_project_diskusinow.adapter.RoomAdapter
 import com.dev.final_project_diskusinow.databinding.ActivityRoomInformationBinding
+import com.dev.final_project_diskusinow.utils.Result
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class RoomInformationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRoomInformationBinding
     private val viewModel: RoomViewModel by viewModels() {
         ViewModelFactory.getInstance(this)
     }
+    private val adapter = RoomAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRoomInformationBinding.inflate(layoutInflater)
@@ -20,8 +27,27 @@ class RoomInformationActivity : AppCompatActivity() {
             onBackPressed()
         }
         viewModel.getAllRoom()
-    }
+        lifecycleScope.launch {
+            viewModel.dataRoom.collect { result ->
+                when (result) {
+                    is Result.Loading -> {
 
+                    }
+                    is Result.Success -> {
+                        adapter.submitList(result.data)
+                    }
+                    is Result.Error -> {
+                        result.error?.let { showErrorSnackbar(it) }
+                    }
+                }
+            }
+        }
+        binding.rvRoom.layoutManager = LinearLayoutManager(this)
+        binding.rvRoom.adapter = adapter
+    }
+    private fun showErrorSnackbar(errorMessage: String) {
+        Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_LONG).show()
+    }
     override fun onBackPressed() {
         super.onBackPressed()
     }
--- app/src/main/java/com/dev/final_project_diskusinow/adapter/RoomAdapter.kt
+++ app/src/main/java/com/dev/final_project_diskusinow/adapter/RoomAdapter.kt
@@ -1,4 +1,47 @@
 package com.dev.final_project_diskusinow.adapter
 
-class RoomAdapter {
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.dev.final_project_diskusinow.data.network.response.DataItem
+import com.dev.final_project_diskusinow.databinding.ItemRoomBinding
+
+class RoomAdapter :  ListAdapter<DataItem, RoomAdapter.ViewHolder>(DIFF_CALLBACK) {
+    class ViewHolder(private val binding: ItemRoomBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(room: DataItem) {
+            binding.apply {
+                tvNameRoom.text = room.name
+                tvFloorRoom.text = "Lantai ${room.floor}"
+                tvCapacityRoom.text = "Kapasitas ${room.capacity} Orang"
+
+            }
+        }
+    }
+
+    private fun showSlotRoom() {
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemRoomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val room = getItem(position)
+        holder.bind(room)
+    }
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DataItem>() {
+            override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
